293df61db14ccc00fb3665963d1168660cd86d9f
YiZou89 <zouyi89@outlook.com>
2023-09-10 11:09:35 +0800

    day1

---
+++ client/client.go
@@ -0,0 +1,38 @@
+package client
+
+import (
+	"io"
+	"sync"
+
+	"github.com/joeyz1729/geerpc/codec"
+)
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
+
+type Client struct {
+	cc       codec.Codec
+	opt      *Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+// 检查方法是否实现
+var _ io.Closer = (*Client)(nil)
+
+
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joeyz1729/geerpc
+
+go 1.20
